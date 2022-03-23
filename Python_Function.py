5eb6fe2abb7362edfdb6a9e6c607fe207930bd98
MaryAyobami <ogunmolamaryayobami@gmail.com>
2022-03-23 01:45:09 +0100

    a function which returns greetings

---
+++ Python_Function.py
@@ -0,0 +1,9 @@
+# a function which accepts name and return a greeting
+
+def greetings(name):
+    greet_phrase = f"Hello,{name} have a wonderful day ahead!"
+    return greet_phrase
+
+#testing
+Test1 = greetings("Ayobami")
+print(Test1)
